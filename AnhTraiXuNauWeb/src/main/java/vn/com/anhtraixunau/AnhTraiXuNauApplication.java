aef7f49cae5750f1106b2bc6bec5623560400e16
Hoa Truong <huynhhoapy97@gmail.com>
2022-02-25 17:40:55 +0700

    Create new project

---
+++ AnhTraiXuNauWeb/src/main/java/vn/com/anhtraixunau/AnhTraiXuNauApplication.java
@@ -0,0 +1,11 @@
+package vn.com.anhtraixunau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnhTraiXuNauApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AnhTraiXuNauApplication.class, args);
+	}
+}
